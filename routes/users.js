3b6e8769d284d55cf01d1eaf8ff0a2e35c6d76b5
NoeliaRominaAcosta <noeliaromina20@gmail.com>
2022-04-28 15:38:01 -0300

    home finished

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+/* const express = require('express');
+const router = express.Router();
+const {login, register} = require('../controllers/indexController') */
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-module.exports = router;
+/* /users */
+/* router.get('/register', register);
+router.get('/login', login);
+
+module.exports = router; */
