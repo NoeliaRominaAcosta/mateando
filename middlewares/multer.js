1d5488ed4bc84b8f8dafb75f197c5448fa4fd84c
NoeliaRominaAcosta <noeliaromina20@gmail.com>
2022-06-16 10:23:08 -0300

    css users ready

---
+++ middlewares/multer.js
@@ -0,0 +1,18 @@
+
+const path = require('path')
+const multer = require('multer');
+
+
+const storage = multer.diskStorage({
+    destination : (req,file,callback) => {
+        callback(null,'public/images')
+    },
+    filename : (req,file,callback) => {
+        callback(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+const upload = multer({
+    storage
+})
+
+module.exports = upload;
--- routes/products.js
+++ routes/products.js
@@ -1,23 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path')
-const multer = require('multer');
+const upload = require('../middlewares/multer')
 const productsValidations = require('../validations/productValidations')
 const {products, detail, getByCategory, store, add, edit, update, remove} = require('../controllers/productsController')
 
 
 /* MULTER */
-const storage = multer.diskStorage({
-    destination : (req,file,callback) => {
-        callback(null,'public/images')
-    },
-    filename : (req,file,callback) => {
-        callback(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-    }
-})
-const upload = multer({
-    storage
-})
 /* GET /product page. */
 
 router
